1db1e334c3d37cc300432232775f73f8402bedfa
thiagotnunes08 <thiagotomaznunes.9908@outlook.com>
2025-09-16 10:44:07 -0300

    initial commit

---
+++ src/main/java/br/com/anti/fraude/service/FraudDetectionEventDrivenApplication.java
@@ -0,0 +1,13 @@
+package br.com.anti.fraude.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionEventDrivenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionEventDrivenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/anti/fraude/service/FraudDetectionEventDrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anti.fraude.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionEventDrivenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
